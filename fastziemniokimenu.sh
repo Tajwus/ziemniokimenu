1fe9a8d8c7b5f1b94b7b59254cf656dc7ebf09eb
Tajwus <129999636+Tajwus@users.noreply.github.com>
2023-05-09 19:30:27 +0200

    Update for v1

--- fastziemniokimenu.sh
+++ fastziemniokimenu.sh
@@ -13,7 +13,7 @@ while true; do
     case $download in
         "1.17" | "1.17.1" | "1.18" | "1.18.1" | "1.18.2" | "1.19" | "1.19.1" | "1.19.2" | "1.19.3" | "1.19.4")
             sudo apt update
-            yes | sudo apt install htop screen wget openjdk-17-jdk
+            yes | sudo apt install -y htop screen wget openjdk-17-jdk
             wget https://download.getbukkit.org/spigot/spigot-$download.jar
 
             wget https://github.com/Tajwus/ziemniokimenu/archive/refs/tags/Update_v1.tar.gz
